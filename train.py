9b609840c3dc1fe40b629529f39ce31c66a16ad4
Yecis <2529971917@qq.com>
2026-07-20 21:53:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
 if __name__ == '__main__':
     model = YOLO('ultralytics/cfg/models/multimodal/FreQNet.yaml')
     # model.load('yolo11n.pt') # loading pretrain weights
-    # model = YOLO('runs/OGSOD/train/512/FCAIQNet/weights/last.pt') ## Resume traing setting
+    # model = YOLO('runs/OGSOD/train/512/FreQNet/weights/last.pt') ## Resume traing setting
     model.train(data='dataset/DroneVehicle.yaml',
                 cache=False,
                 imgsz=640,
